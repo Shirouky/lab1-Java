e273d4a0cda1421bacd7ab41506163ef9ce469d8
Shirouky <83469591+Shirouky@users.noreply.github.com>
2025-03-10 21:19:37 +0300

    Working programm

---
+++ src/main/java/org/main/Controller.java
@@ -0,0 +1,12 @@
+package org.main;
+import org.data.Results;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class System {
+    HashMap<String, ArrayList<Double>> data = new HashMap<String, ArrayList<Double>>();
+    Results results = new Results();
+
+
+}
